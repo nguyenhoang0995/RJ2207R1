7bdb7dad20a7fb0d57b51151246feb1d378dc017
Ros <nguyenhoang24091995@gmail.com>
2022-08-22 19:38:12 +0700

    update2

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <p>Nguyễn Nguyên Hoàng2</p>
 );
 
 // If you want to start measuring performance in your app, pass a function
