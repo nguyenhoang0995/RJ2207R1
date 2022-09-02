1348ba9c9a5e6a1c77ae459b8236ed52cd173505
Ros <nguyenhoang24091995@gmail.com>
2022-09-02 15:51:42 +0700

    update1

---
+++ src/Componets/Headline.js
@@ -0,0 +1,7 @@
+function Headline({title, className}){
+
+    return(
+        <h1>{className} npmThis is a Headline {title}</h1>
+    )
+}
+export default Headline;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Headline from './Componets/Headline';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <p>Nguyễn Nguyên Hoàng2</p>
-);
+<h1>nguyen nguyen hoang24091995</h1>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <p>Nguyễn Nguyên Hoàng2</p>
+// );
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<Headline className="Newsletter" title="Newsletter" />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
